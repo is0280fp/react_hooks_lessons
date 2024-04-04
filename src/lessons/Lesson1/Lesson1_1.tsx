97715e4a67d4f280f5fcd7f9d07b9bd175ec7cd6
is0280fp <umeyume45@gmail.com>
2024-04-04 10:12:52 +0900

    Finished lesson1-1 and lesson1-2

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,22 @@
+import { ChangeEvent, useState } from "react";
+
 const Lesson1_1 = () => {
+  const [age, setAge] = useState<number>(0);
+  const [name, setName] = useState("Shincode")
+  console.log("rendering")
+
+  const handleClick = () => {
+    setAge(preAge => preAge + 1);
+    setAge(preAge => preAge + 1);
+    setAge(preAge => preAge + 1);
+    console.log(name)
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <input type="text" value={name} onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}/>
+      <button className="border p-2 rounded-md bg-red-100" onClick={handleClick}>Add Age</button>
+      <p>You are {age}</p>
     </div>
   );
 };
